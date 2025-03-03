c275dbce6c2111621e7c40b9306520b815c79e92
kalaiselviThiru <kkalaiselvi011325@gmail.com>
2025-03-03 11:37:42 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,1 +1,11 @@
+package com.example;
 
+public class Main {
+    public String greet() {
+        return "Hello, Jenkins!"; 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().greet()); 
+    }
+}
